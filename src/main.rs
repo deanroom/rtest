31b69310a21a90c5a804030a228787710a3beea2
JerryDing <deanroom@gmail.com>
2024-06-26 09:43:52 +0800

    增加各种参数

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+use std::env;
 use std::io::Write;
-use std::simd::LaneCount;
 use std::time::{Duration, Instant};
 use std::{io, thread};
 struct PeriodInfo {
     next_period: Instant,
     period_ns: u64,
+    sleep_duration: Duration,
 }
 
 impl PeriodInfo {
@@ -12,20 +13,25 @@ impl PeriodInfo {
         PeriodInfo {
             next_period: Instant::now(),
             period_ns,
+            sleep_duration: Duration::from_nanos(0),
         }
     }
 
-    fn inc_period(&mut self) {
+    fn inc_period(&mut self, time: Instant) {
+        self.next_period = time + Duration::from_nanos(self.period_ns);
+    }
+    fn inc_period_continuous(&mut self) {
         self.next_period += Duration::from_nanos(self.period_ns);
     }
 
     fn wait_rest_of_period(&mut self) {
-        self.inc_period();
-        // let now = Instant::now();
-        // let sleep_duration = self.next_period.duration_since(now);
-        // thread::sleep(sleep_duration);
+        let now = Instant::now();
+        self.sleep_duration = self.next_period.duration_since(now);
+        thread::sleep(self.sleep_duration);
+    }
+    fn spin_rest_of_period(&mut self) {
         while self.next_period > Instant::now() {
-            // thread::yield_now();
+            thread::yield_now();
         }
     }
 }
@@ -36,7 +42,7 @@ fn periodic_task_init(pinfo: &mut PeriodInfo) {
 }
 
 fn do_rt_task() {
-    thread::sleep(Duration::from_nanos(100_000));
+    // thread::sleep(Duration::from_nanos(100_000));
     // Do RT stuff here.
     // Placeholder for real-time task logic
     // println!("Doing real-time task");
@@ -90,20 +96,67 @@ impl DurationStats {
     }
 }
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut mode = Mode::Spin;
+    if args.len() != 2 {
+        // println!("Usage: {} <mode>", args[0]);
+        println!("Use default mode: spin");
+    } else {
+        mode = match args[1].as_str() {
+            "spin" => Mode::Spin,
+            "sleep" => Mode::Sleep,
+            "clock" => Mode::Clock,
+            _ => {
+                println!("Invalid mode");
+                return;
+            }
+        };
+    }
+
+    start_test(mode);
+}
+enum Mode {
+    Spin,
+    Sleep,
+    Clock,
+}
+
+impl PartialEq for Mode {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Mode::Spin, Mode::Spin) => true,
+            (Mode::Sleep, Mode::Sleep) => true,
+            (Mode::Clock, Mode::Clock) => true,
+            _ => false,
+        }
+    }
+}
+fn start_test(mode: Mode) -> ! {
     let mut pinfo = PeriodInfo::new(0);
     periodic_task_init(&mut pinfo);
     let mut stats = DurationStats::new();
-    let mut count: u64 = 0;
+    let mut count: u16 = 0;
     loop {
         let before = Instant::now();
+        if mode == Mode::Clock {
+            pinfo.inc_period_continuous();
+        } else {
+            pinfo.inc_period(before);
+        }
+        count += 1;
         do_rt_task();
-        pinfo.wait_rest_of_period();
-        let after = Instant::now();
-        let duration = (after - before).as_micros();
+
+        match mode {
+            Mode::Spin => pinfo.spin_rest_of_period(),
+            Mode::Sleep => pinfo.wait_rest_of_period(),
+            Mode::Clock => pinfo.spin_rest_of_period(),
+        }
+
+        let duration = (Instant::now() - before).as_micros();
         stats.update(duration);
-        if count >= 10_0000 {
-            break;
+        if count == 1000 {
+            stats.print_stats(duration);
+            count = 0;
         }
     }
-    stats.print_stats(1000);
 }
